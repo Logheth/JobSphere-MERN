7d2ba90c291724c411c8e939eb24cd709d8acea4
Logheth <logheth18@gmail.com>
2025-02-08 17:34:29 +0530

    Updated db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ dbConnect()
 async function dbConnect(){
 
      try {
-         await mongoose.connect('mongodb+srv://lm0458:logheth@cluster0.hun4b.mongodb.net/MERN_JOB');
+         await mongoose.connect('mongodb+srv://lm0458:*******@cluster0.hun4b.mongodb.net/MERN_JOB');
          console.log('Mongo DB Connection success')
      } catch (error) {
          console.log('Mongo DB Connection failed')
